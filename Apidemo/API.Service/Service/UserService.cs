66090daaba37d959a8a74317b1a52a471c584d96
tamht <htam37@gmail.com>
2019-01-29 18:13:36 +0700

    ht

---
+++ Apidemo/API.Service/Service/UserService.cs
@@ -0,0 +1,17 @@
+﻿using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Service.Service
+{
+	public class UserService
+	{
+		private ProductEntities1 db = new ProductEntities1();
+		public User GetUser(string username, string password )
+		{
+			return db.Users.Where(x => x.username.Equals(username) && x.password.Equals(password)).FirstOrDefault();
+		}
+	}
+}
