47b068b621213ef4454ceeafda50de1c27d668f2
Hoàng Thị Tâm <htam37@gmail.com>
2019-01-30 18:00:48 +0700

    Update authInterceporService.js

--- Apidemo/API.Web/Service/authInterceporService.js
+++ Apidemo/API.Web/Service/authInterceporService.js
@@ -1,5 +1,5 @@
-﻿'use strict';
-myApp.factory('authInterceptorService', ['$q', '$injector', '$location', 'localStorageService', function ($q, $injector, $location, localStorageService) {
+'use strict';
+myApp.factory('authInterceptorService', ['$q', '$location', 'localStorageService', function ($q, $location, localStorageService) {
 
     var authInterceptorServiceFactory = {};
 
@@ -17,16 +17,6 @@ myApp.factory('authInterceptorService', ['$q', '$injector', '$location', 'localS
 
     var _responseError = function (rejection) {
         if (rejection.status === 401) {
-            var authService = $injector.get('authService');
-            var authData = localStorageService.get('authorizationData');
-
-            if (authData) {
-                if (authData.useRefreshTokens) {
-                    $location.path('/refresh');
-                    return $q.reject(rejection);
-                }
-            }
-            authService.logOut();
             $location.path('/login');
         }
         return $q.reject(rejection);
